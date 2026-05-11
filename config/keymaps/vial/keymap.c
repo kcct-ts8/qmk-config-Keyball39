c067375c5ed885148afe58e05eb97fc05b9da485
kcct-ts8 <43575586+kcct-ts8@users.noreply.github.com>
2026-05-12 07:17:34 +0900

    Update keymap.c

--- config/keymaps/vial/keymap.c
+++ config/keymaps/vial/keymap.c
@@ -154,9 +154,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //   EE_CLR   = EEPROM全クリア (DPI最小値変更後に1回押す)
     // =========================================================
     [LAYER_RGB] = LAYOUT(
-        RGB_TOG,  RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW,        _______,  _______,  _______,  _______,  T_SAVE,
+        RGB_TOG,  RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW,        QK_BOOT,  _______,  TInfo,    _______,  T_SAVE,
 
-        RGB_MOD,  RGB_HUI,  RGB_SAI,  RGB_VAI,  RGB_M_SN,        _______,  S_D_MOD,  _______,  _______,  DRG_TOG,
+        RGB_MOD,  RGB_HUI,  RGB_SAI,  RGB_VAI,  RGB_M_SN,        RGB_MSP,  S_D_MOD,  _______,  _______,  DRG_TOG,
 
         RGB_RMOD, RGB_HUD,  RGB_SAD,  RGB_VAD,  RGB_M_K,         DPI_RMOD, EE_CLR,   _______,  DPI_MOD,  _______,
 
