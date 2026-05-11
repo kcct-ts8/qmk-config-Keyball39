22677270a944f8b349dbde398c81a8d3760f8e8b
kcct-ts8 <43575586+kcct-ts8@users.noreply.github.com>
2026-05-12 05:51:22 +0900

    Update keymap.c
    Add bootloader key

--- config/keymaps/vial/keymap.c
+++ config/keymaps/vial/keymap.c
@@ -110,34 +110,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,        KC_N,        KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,
 
-        LT(LAYER_NUMBER, KC_LALT), LT(LAYER_RGB, KC_BSPC), LT(LAYER_SYMBOL, KC_ESC), LT(1, KC_LALT), LT(LAYER_MOUSE, KC_SPC), LT(LAYER_NUMBER, KC_SPC),         LT(LAYER_NUMPAD, KC_MINS), LT(LAYER_SYMBOL, KC_ENT), LT(LAYER_RGB, KC_LBRC)
+        LT(LAYER_RGB, KC_BSPC), LT(LAYER_SYMBOL, KC_ESC), TG(LAYER_NUMBER), LSFT_T(KC_LNG2), LT(LAYER_MOUSE, KC_SPC), KC_LALT,
+        LT(LAYER_NUMPAD, KC_MINS), LT(LAYER_SYMBOL, KC_ENT), LT(LAYER_RGB, KC_LBRC)
     ),
 
     // =========================================================
     // Layer 1: Mouse / Media
-    // L5ホールドで起動, L6位置のTG(MOUSE)でロック(ホールド解放後も維持)
-    // SNIPING (Dキー) = 低DPI精密モード (ホールド中のみ)
+    // L5(Space)ホールドで一時起動 (離すと自動解除)
+    // SNIPING (Sキー位置) = 低DPI精密モード (ホールド中のみ)
     // DRGSCRL (Kキー位置) = ドラッグスクロールモード (ホールド)
     // =========================================================
     [LAYER_MOUSE] = LAYOUT(
-        SGUI(KC_1),       SGUI(KC_2),       LCTL(SGUI(KC_3)), LCTL(SGUI(KC_4)),        LCTL(KC_T),     KC_F17,           KC_F19,           KC_F18,           KC_F13,    KC_F12,    
+        SGUI(KC_1), SGUI(KC_2), LCTL(SGUI(KC_3)), LCTL(SGUI(KC_4)), LCTL(KC_T),         KC_F17,  KC_F19,     KC_F18,  KC_F13,   KC_F12,
 
-        _______,          SNIPING,          LALT(KC_C),       LCTL(SGUI(KC_D)),        KC_F15,         KC_BSPC,          KC_MS_BTN1,       DRGSCRL,          KC_MS_BTN2,  KC_F16,    
+        _______,    SNIPING,    LALT(KC_C),       LCTL(SGUI(KC_D)), KC_F15,             KC_BSPC, KC_MS_BTN1, DRGSCRL, KC_MS_BTN2, KC_F16,
 
-        _______,          _______,          _______,          LCTL(LALT(KC_SPC)),        KC_VOLD,        KC_VOLU,          KC_MS_BTN3,       KC_MUTE,          _______, _______,
- 
+        _______,    _______,    _______,          LCTL(LALT(KC_SPC)), KC_VOLD,          KC_VOLU, KC_MS_BTN3, KC_MUTE, _______,    _______,
+
         _______, _______, _______, _______, _______, _______,        _______, _______, _______
     ),
 
     // =========================================================
-    // Layer 2: Symbol
+    // Layer 2: Symbol & Arrows (Keyball 44から移植)
+    //
+    // 【左手】HJKLスタイル矢印 (ホームロー)
+    //   A=←, S=↓, D=↑, F=→
+    // 【左手内側列】
+    //   T=] (Shiftで})
+    //   G=\ (Shiftで|)
+    // 【右手】Mac風矢印 (ホームロー)
+    //   H=BS, J=←, K=↑, L=↓, ;=→
+    // 【右手記号】
+    //   Y=; (Shiftで:)
+    //   U=_, I=&, O=@, P=^
+    // 【右手数値演算】
+    //   N=+, M=-, ,==, .=*
     // =========================================================
     [LAYER_SYMBOL] = LAYOUT(
-        _______, _______, _______, _______, _______,        _______, S(KC_INT1), S(KC_7), S(KC_8), S(KC_9),
+        _______, _______, _______, _______, KC_RBRC,        KC_SCLN, S(KC_INT1), S(KC_7), S(KC_2), S(KC_6),
 
-        _______, _______, _______, _______, _______,        KC_BSPC, KC_PLUS, KC_MINS, S(KC_BSLS), S(KC_0),
+        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_BSLS,       KC_BSPC, KC_LEFT,    KC_UP,   KC_DOWN, KC_RIGHT,
 
-        _______, _______, _______, _______, _______,        KC_PPLS, KC_PMNS, _______, KC_PAST, _______,
+        _______, _______, _______, _______, _______,        KC_PPLS, KC_PMNS,    KC_PEQL, KC_PAST, _______,
 
         _______, _______, _______, _______, _______, _______,        _______, _______, _______
     ),
@@ -151,9 +165,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //   DRG_TOG  = ドラッグスクロールトグル
     //   T_SAVE   = ユーザー設定をEEPROMに保存
     //   EE_CLR   = EEPROM全クリア (DPI最小値変更後に1回押す)
+    //   QK_BOOT  = ブートローダーモードへ移行 (Yキー位置)
     // =========================================================
     [LAYER_RGB] = LAYOUT(
-        RGB_TOG,  RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW,        _______,  _______,  _______,  _______,  T_SAVE,
+        RGB_TOG,  RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW,        QK_BOOT,  _______,  _______,  _______,  T_SAVE,
 
         RGB_MOD,  RGB_HUI,  RGB_SAI,  RGB_VAI,  RGB_M_SN,        _______,  S_D_MOD,  _______,  _______,  DRG_TOG,
 
@@ -167,26 +182,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // L3(TG(NUMBER))で出入り
     // =========================================================
     [LAYER_NUMBER] = LAYOUT(
-        KC_1,  KC_2,  KC_3,  KC_4, KC_5,                  KC_6,  KC_7,  KC_8,  KC_9, KC_0,
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
 
-        KC_F1, KC_F2, KC_F3, KC_F4,KC_F5,                 KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,               KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
 
-        _______, _______, _______, _______,  _______,     _______, _______, _______, _______, _______, 
+        _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______,
 
-        _______, _______, _______, _______, _______, _______,      _______, _______, _______
+        _______, _______, _______, _______, _______, _______,    _______, _______, _______
     ),
 
     // =========================================================
     // Layer 5: Numpad
     // =========================================================
     [LAYER_NUMPAD] = LAYOUT(
-        _______, _______, _______, _______, _______,            KC_PEQL, KC_P1,   KC_P2,   KC_P3,   KC_PPLS,
+        _______, _______, _______, _______, _______,        KC_PEQL, KC_P1,   KC_P2,   KC_P3,   KC_PPLS,
 
-        _______, _______, _______, _______, _______,            KC_PMNS, KC_P4,   KC_P5,   KC_P6,   KC_PMNS,
+        _______, _______, _______, _______, _______,        KC_PMNS, KC_P4,   KC_P5,   KC_P6,   KC_PMNS,
 
-        _______, _______, _______, _______, _______,            KC_PAST, KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
+        _______, _______, _______, _______, _______,        KC_PAST, KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
 
-        _______, _______, _______, _______, _______, _______,   KC_P0,   KC_PDOT, _______
+        _______, _______, _______, _______, _______, _______,        KC_P0,   KC_PDOT, _______
     ),
 };
 
