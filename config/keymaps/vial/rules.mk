be07e978df8c169cec4c338cdf2a260e97fb22c0
kcct-ts8 <43575586+kcct-ts8@users.noreply.github.com>
2026-05-12 13:42:50 +0900

    Update rules.mk

--- config/keymaps/vial/rules.mk
+++ config/keymaps/vial/rules.mk
@@ -9,8 +9,3 @@ WPM_ENABLE = yes
 ENCODER_ENABLE = no
 ENCODER_MAP_ENABLE = no
 
-VIAL_COMBO_ENABLE = no    # Vialの動的コンボを無効化
-COMBO_ENABLE = yes        # QMK標準のコンボシステムを有効化(これは元から有効かも)
-
-# Vialのkey_combos定義を強制的に抑止 (フォークがVIAL_COMBO_ENTRIESを使う場合用)
-OPT_DEFS += -DVIAL_COMBO_ENTRIES=0
